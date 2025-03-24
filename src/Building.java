8b830b3bba41ddb1b0b439f275c57702d0c1d7a0
brian-hilton <brian.hilton67@gmail.com>
2025-03-23 23:32:42 -0500

    Add getters/setters

--- src/Building.java
+++ src/Building.java
@@ -2,4 +2,26 @@ public class Building {
     private int height;
     private int numStories;
     private String name;
+
+    public int getHeight() {
+        return height;
+    }
+    public int getNumStories() {
+        return numStories;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setNumStories(int numStories) {
+        this.numStories = numStories;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
